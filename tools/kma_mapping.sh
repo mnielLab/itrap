897f6e26b225e2d6a84f35269d61177573204385
herpov <herpov@dtu.dk>
2019-11-11 18:07:21 +0100

    Edit to fit snakemake

--- tools/kma_mapping.sh
+++ tools/kma_mapping.sh
@@ -1,9 +1,11 @@
-ARGS="-1t1"
-KMA_DIR="/home/tuba/herpov/tcr-pmhc-sc-project/data/exp3_MHC/kma/kma"$(echo -e "${ARGS}" | tr -d '[:space:]')
-BARCODE_TEMPLATES="/home/tuba/herpov/tcr-pmhc-sc-project/data/exp3_MHC/barcode_library/barcode_templates.fa"
-READS="/home/tuba/herpov/tcr-pmhc-sc-project/data/exp3_MHC/processed/longranger_clean/IONTORRENT.R1.gems.no_umi.no_adapters.fq"
+ARGS=$3 #"-1t1" $(echo -e "${ARGS}" | tr -d '[:space:]')
+
+BARCODE_TEMPLATES=$1 #"/home/tuba/herpov/tcr-pmhc-sc-project/data/exp3_MHC/barcode_library/barcode_templates.fa"
+READS=$2 #"/home/tuba/herpov/tcr-pmhc-sc-project/data/exp3_MHC/processed/longranger_clean/IONTORRENT.R1.gems.no_umi.no_adapters.fq"
 
 KMA=/home/tuba-nobackup/shared/src/kma/kma
+
+KMA_DIR=$(echo $4 | rev | cut -d"/" -f3- | rev) 
 
 mkdir -p $KMA_DIR/output
 
