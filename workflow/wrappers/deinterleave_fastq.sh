7a73fe2c0b51c50a7bdbb589245f703701007cd3
herpov <herpov@dtu.dk>
2023-04-24 19:49:55 +0200

    removed workflow structure

--- workflow/wrappers/deinterleave_fastq.sh
+++
@@ -1,7 +0,0 @@
-interleaved_fq=$1
-read1_fq=$2
-read2_fq=$3
-
-zcat $interleaved_fq | paste - - - - - - - - | grep "BX:Z:" \
-    | tee >(cut -f 1-4 | tr "\t" "\n" | gzip > $read1_fq) \
-    |       cut -f 5-8 | tr "\t" "\n" | gzip > $read2_fq
--- workflow/wrappers/wrt_dummy_reads.sh
+++
@@ -1,1 +0,0 @@
-zcat $1 | awk --posix '{ if (NR % 4 == 0) { sub(/.*/,"CCCCCCCC")} else if (NR % 2 == 0) { sub(/.*/,"NNNNNNNN")}; print}' | gzip > $2
